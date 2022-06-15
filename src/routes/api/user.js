6877b2fbfb6b60c720b225d230fbdd8059eeead4
SuperGandhi <patrickborjabueno@hotmail.com>
2022-06-15 15:28:38 -0500

    routes and structure general

---
+++ src/routes/api/user.js
@@ -0,0 +1,18 @@
+const {Router} = require('express')
+
+const route = Router()
+
+route.get('/', (req, res)=>{
+    res.send({
+        "user": {
+            "email": "jake@jake.jake",
+            "toke": "jwt.token.here",
+            "username": "jake",
+            "bio": "I work at statefarm",
+            "image" : null
+        }
+    })
+})
+
+
+module.exports= route
